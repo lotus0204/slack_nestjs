d1139ac998c1ca6083b63cecca7df7191ead3fb1
lotus0204 <lotusrb0204@gmail.com>
2021-12-26 01:39:34 +0900

    add channels

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DmsModule } from './dms/dms.module';
 import { ChannelsModule } from './channels/channels.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, DmsModule, ChannelsModule],
+  imports: [ConfigModule.forRoot(), UsersModule, DmsModule, ChannelsModule, WorkspacesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/channels/channels.controller.ts
+++ src/channels/channels.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Query, Param, Body } from '@nestjs/common';
 
 @Controller('api/workspaces/:url/channels')
-export class ChannelsController {}
+export class ChannelsController {
+  @Get()
+  getAllChannels() { }
+  
+  @Post()
+  createChannels() { }
+
+  @Get()
+  getSpecificChannels(){}
+
+  @Get(':name/chats')
+  getChats(@Query() query, @Param() param) {
+    console.log(query.perPage, query.page);
+    console.log(param.id, param.url);
+    // [@Query('perPage') perPage] 와 같이 쿼리 안에 속성을 넣어줘서 받을 수 있다. 
+  }
+
+  @Post(':name/chats')
+  postChat(@Body() content) {
+    
+  }
+
+  
+
+}
